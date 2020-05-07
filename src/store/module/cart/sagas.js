d411488ed179c6918e3952908a29cd17ee279b87
Felipe Leal <felipewanderson2020@gmail.com>
2020-05-06 22:39:12 -0300

    separated actions with saga

--- src/store/module/cart/sagas.js
+++ src/store/module/cart/sagas.js
@@ -1,11 +1,27 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, put, select, all, takeLatest } from 'redux-saga/effects';
 import api from '../../../services/api';
-import { addToCartSuccess } from './actions';
+import { addToCartSuccess, updateAmonut } from './actions';
+import { formatPrice } from '../../../utils/format';
 
 function* addToCart({ id }) {
-  const response = yield call(api.get, `products/${id}`);
+  const productExistis = yield select((state) =>
+    state.cart.find((p) => p.id === id)
+  );
 
-  yield put(addToCartSuccess(response.data));
+  if (productExistis) {
+    const amount = productExistis.amount + 1;
+
+    yield put(updateAmonut(id, amount));
+  } else {
+    const response = yield call(api.get, `products/${id}`);
+
+    const data = {
+      ...response.data,
+      amount: 1,
+      priceFormatted: formatPrice(response.data.price),
+    };
+    yield put(addToCartSuccess(data));
+  }
 }
 
 export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
